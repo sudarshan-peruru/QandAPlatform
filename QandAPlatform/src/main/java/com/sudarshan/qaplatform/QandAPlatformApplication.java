6f00d4896fad0adcf8a618c825e880721f24c4c8
sudarshan-peruru <43790110+sudarshan-peruru@users.noreply.github.com>
2021-04-07 10:39:45 +0530

    Initial Project Creation

---
+++ QandAPlatform/src/main/java/com/sudarshan/qaplatform/QandAPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sudarshan.qaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QandAPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QandAPlatformApplication.class, args);
+	}
+
+}
---
+++ QandAPlatform/src/test/java/com/sudarshan/qaplatform/QandAPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.qaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QandAPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
